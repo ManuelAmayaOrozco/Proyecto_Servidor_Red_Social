9370735da6b322bfa8fbb419d2f87a5d8c37b2d3
ManuelAmayaOrozco <mamaoro1405@g.educaand.es>
2025-02-25 18:56:23 +0100

    Posts Arreglados

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -58,7 +58,7 @@ class PostController extends Controller
         $post->belongs_to = Auth::id();
         $post->save();
 
-        return $this->showPosts();
+        return redirect()->route('home');
 
     }
 
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [PostController::class, 'showPosts'])->name('post.showPosts');
+Route::get('/', [PostController::class, 'showPosts'])->name('home');
 
 
 Route::prefix('users')->group(base_path('routes/users/users.php'));
